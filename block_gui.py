963e0137e7ca70f09c7a07cd98ef7675af150971
xguenard <xavier.guenard@gmail.com>
2016-08-08 02:27:40 +0200

    Small changes

--- block_gui.py
+++ block_gui.py
@@ -31,28 +31,36 @@ class BlockchainWindow(QtGui.QWidget):
 
         try:
             self.draw_rectangle(qp, self.blocks[-1]\
-                    , size.height()/10)
+                    , size.height()/10, len(self.blocks)-1)
         except:
             pass
 
         try:
             y = 2*size.height()/6 + size.height()/10
-            self.draw_rectangle(qp, self.blocks[-2], y)
+            self.draw_rectangle(qp, self.blocks[-2], y, len(self.blocks)-2)
         except:
             pass
 
         try:
             y = 4 *size.height()/6 + size.height()/10
-            self.draw_rectangle(qp, self.blocks[-3], y)
+            self.draw_rectangle(qp, self.blocks[-3], y, len(self.blocks)-3)
         except:
             pass
 
-    def draw_rectangle(self, qp, elems, y):
+    def draw_rectangle(self, qp, elems, y, blk_id):
+        size = self.size()
+        rect1 = QtCore.QRectF(0, y, size.width()/5.1, size.height()/6)
+
         elems_txt = "\n".join([e.short_str() \
                 for e in elems])
-        size = self.size()
-        rect = QtCore.QRectF(size.width()/10, y\
-                ,size.width()/1.1, size.height()/6)
+        if not elems:
+            elems_txt = "EMPTY BLOCK"
+        rect = QtCore.QRectF(size.width()/5, y\
+                ,size.width()/1.2, size.height()/6)
+
         qp.setBrush(QtGui.QColor(36, 145, 181))
         qp.drawRect(rect)
+        qp.setBrush(QtGui.QColor(8, 67, 92))
+        qp.drawRect(rect1)
         qp.drawText(rect, QtCore.Qt.AlignCenter, elems_txt)
+        qp.drawText(rect1, QtCore.Qt.AlignCenter, "block : {}".format(blk_id))
--- mvt.py
+++
@@ -1,2 +0,0 @@
-#Definition of an mvt, atomic element of the blockchain.
-#I create an mvt when an order is validate by the blockchain
